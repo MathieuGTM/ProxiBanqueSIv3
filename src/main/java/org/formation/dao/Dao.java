a440cf6ea3a66f9b06404b1202d11bd1341707fe
Mathieu <mateul@live.fr>
2017-03-30 13:25:37 +0200

    changement DAO

--- src/main/java/org/formation/dao/Dao.java
+++ src/main/java/org/formation/dao/Dao.java
@@ -1,5 +1,126 @@
 package org.formation.dao;
 
-public class Dao {
+import java.util.List;
 
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+@Named
+public class Dao<T> implements IDao<T>{
+
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-pu");
+
+
+	
+	public void delete(Class<T> t, long id) {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction etxn = em.getTransaction();
+		try {
+			etxn.begin();
+			T remove = em.find(t, id);
+			
+			em.remove(remove);
+			System.out.println("delete "+remove.toString());
+			etxn.commit();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			if (etxn != null)
+				etxn.rollback();
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		
+	}
+
+
+	public void create(T t) {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction etxn = em.getTransaction();
+		try {
+			etxn.begin();
+			em.persist(t);
+			etxn.commit();
+		} catch (Exception e) {
+			if (etxn != null)
+				etxn.rollback();
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		
+	}
+
+
+	public T select(Class<T> t, long id) {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction etxn = em.getTransaction();
+		T recup = null;
+
+		try {
+			etxn.begin();
+			
+			recup = em.find(t, id);
+			etxn.commit();
+		} catch (Exception e) {
+			if (etxn != null)
+				etxn.rollback();
+			System.out.println(e.getMessage());
+			
+		}finally {
+			if (em != null)
+				em.close();
+		}
+		return recup;
+	}
+
+
+	public List<T> selectAll(Class<T> t,String table) {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction etxn = em.getTransaction();
+		List<T> list = null ;
+		String sql = "SELECT c FROM "+ table+" c ";
+        
+		try {
+			etxn.begin();
+			TypedQuery<T> query = em.createQuery(sql, t);
+			list = query.getResultList();
+			etxn.commit();
+		} catch (Exception e) {
+			if (etxn != null)
+				etxn.rollback();
+			System.out.println(e.getMessage());
+			
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		return list;
+	}
+
+
+	public void update(Class<T> t) {
+		// TODO Auto-generated method stub
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction etxn = em.getTransaction();
+
+		try {
+			etxn.begin();
+			em.merge(t);
+			etxn.commit();
+		} catch (Exception e) {
+			if (etxn != null)
+				etxn.rollback();
+			System.out.println(e.getMessage());
+			
+		}finally {
+			if (em != null)
+				em.close();
+		}
+		
+	}
 }
--- src/main/java/org/formation/dao/IDao.java
+++ src/main/java/org/formation/dao/IDao.java
@@ -1,5 +1,13 @@
 package org.formation.dao;
 
-public interface IDao {
+import java.util.List;
 
+public interface IDao<T> {
+
+	public void delete(Class<T> t, long id);
+	public void create(T t);
+	public T select(Class<T> t, long id);
+	public List<T> selectAll(Class<T> t, String table);
+	public void update(Class<T> t);
+	
 }
