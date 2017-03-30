d043d0f6d486895e6d3e808a19951c61f9ce9470
Mathieu <mateul@live.fr>
2017-03-30 17:11:54 +0200

    début controlleur

--- src/main/java/org/formation/view/Controller.java
+++ src/main/java/org/formation/view/Controller.java
@@ -1,5 +1,43 @@
 package org.formation.view;
 
-public class Controller {
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
+import org.formation.service.IService;
+
+@Named
+@SessionScoped
+public class Controller<T> implements Serializable {
+
+	private static final long serialVersionUID = 8475399265911125474L;
+	private Logger logger = Logger.getLogger(getClass().getName());
+
+	@Inject
+	IService<T> service;
+	
+	public String addClient(T client) {
+
+		logger.info("Adding client : " + client);
+
+		try {
+
+			// add student to the database
+			service.create(client);
+
+		} catch (Exception exc) {
+			// send this to server logs
+			logger.log(Level.SEVERE, "Error adding client", exc);
+
+			// add error message for JSF page
+			//addErrorMessage(exc);
+
+			return null;
+		}
+
+		return "list-clients?faces-redirect=true";
+	}
 }
