444a0f7c3cb3c0a23e9c01e8d13b47f3583a4464
Aurelie <aurelie.potier@gmail.com>
2017-03-31 14:19:31 +0200

    continue virement

--- src/main/java/org/formation/view/CompteController.java
+++ src/main/java/org/formation/view/CompteController.java
@@ -7,6 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.component.UIData;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -15,66 +16,78 @@ import org.formation.model.Client;
 import org.formation.model.CompteBancaire;
 import org.formation.model.CompteCourant;
 import org.formation.model.CompteEpargne;
+import org.formation.service.IService;
 import org.formation.service.IServiceCompte;
 
-
 /**
- * La classe CompteController est un controleur (pattern MVC). Il permet a  la couche presentation de recuperer des informations 
- * sur des clients, de communiquer avec la bdd et permet la navigation et la redirection a certaines pages. Elle permet le log des 
- * messages d'erreur, warning etc.
+ * La classe CompteController est un controleur (pattern MVC). Il permet a  la
+ * couche presentation de recuperer des informations sur des clients, de
+ * communiquer avec la bdd et permet la navigation et la redirection a certaines
+ * pages. Elle permet le log des messages d'erreur, warning etc.
+ * 
  * @author Mathieu et Aurelie
  */
-
 
 @Named
 public class CompteController implements Serializable {
 
-
 	private static final long serialVersionUID = 1L;
 
+
+	@Inject
+	IService<CompteCourant> serviceCC;
+	
 	private Logger logger = Logger.getLogger(getClass().getName());
 
 	private List<CompteCourant> comptesCourant;
-	
+
 	private List<CompteEpargne> comptesEpargne;
 
-	
-	@Inject
-	IServiceCompte service;
-	
+	private UIData dataTable;
+
+	public UIData getDataTable() {
+		return dataTable;
+	}
+
+	public void setDataTable(UIData dataTable) {
+		this.dataTable = dataTable;
+	}
+
+
 	public CompteController() throws Exception {
 		comptesCourant = new ArrayList<>();
 		comptesCourant.add(new CompteCourant(10000, null, 1000, new Client("Paul", "Roger", "0102030405")));
-		comptesEpargne= new ArrayList<>();
+		comptesEpargne = new ArrayList<>();
 	}
+
+	 public String addCompte(CompteCourant compte) {
 	
+	 logger.info("Ajout du compte : " + compte);
 	
-//	public String addCompte(CompteBancaire compte) {
-//
-//		logger.info("Ajout du compte : " + compte);
-//
-//		try {
-//
-//			service.create(compte);
-//
-//		} catch (Exception exc) {
-//			// send this to server logs
-//			logger.log(Level.SEVERE, "Error adding compte", exc);
-//			
-//			// add error message for JSF page
-//			addErrorMessage(exc);
-//
-//			return "";
-//		}
-//
-//		return "list-clients?faces-redirect=true";
-//	}
+	 try {
 	
+	 serviceCC.create(compte);
+	
+	 } catch (Exception exc) {
+	 // send this to server logs
+	 logger.log(Level.SEVERE, "Error adding compte", exc);
+	
+	 // add error message for JSF page
+	 addErrorMessage(exc);
+	
+	 return "";
+	 }
+	
+	 return "list-clients?faces-redirect=true";
+	 }
+	 
+	 
+
 	public List<CompteCourant> getComptesCourant() {
 		loadComptesCourant();
 		return comptesCourant;
 	}
-	
+
 	public void loadComptesCourant() {
 
 		logger.info("Loading comptes courants");
@@ -83,7 +96,7 @@ public class CompteController implements Serializable {
 
 		try {
 
-			comptesCourant = service.getCCs();
+			comptesCourant = serviceCC.getComCour();
 
 		} catch (Exception exc) {
 			// send this to server logs
@@ -93,37 +106,34 @@ public class CompteController implements Serializable {
 			addErrorMessage(exc);
 		}
 	}
-	
-	public List<CompteEpargne> getComptesEpargne() {
-		loadComptesEpargne();
-		return comptesEpargne;
-	}
-	
-	public void loadComptesEpargne() {
 
-		logger.info("Loading comptes epargne");
+//	public List<CompteEpargne> getComptesEpargne() {
+//		loadComptesEpargne();
+//		return comptesEpargne;
+//	}
+//
+//	public void loadComptesEpargne() {
+//
+//		logger.info("Loading comptes epargne");
+//
+//		comptesEpargne.clear();
+//
+//		try {
+//
+//			comptesEpargne = serviceCC.getCEs();
+//
+//		} catch (Exception exc) {
+//			// send this to server logs
+//			logger.log(Level.SEVERE, "Error ", exc);
+//
+//			// add error message for JSF page
+//			addErrorMessage(exc);
+//		}
+//	}
 
-		comptesEpargne.clear();
-
-		try {
-
-			comptesEpargne = service.getCEs();
-
-		} catch (Exception exc) {
-			// send this to server logs
-			logger.log(Level.SEVERE, "Error ", exc);
-
-			// add error message for JSF page
-			addErrorMessage(exc);
-		}
-	}
-	
-
-	
 	private void addErrorMessage(Exception exc) {
 		FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
 		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
-	
-	
+
 }
